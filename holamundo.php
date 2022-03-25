92d4702230636b9df43e5c86aa6e8b5c07c04800
IvanDanie <102390518+IvanDanie@users.noreply.github.com>
2022-03-25 18:56:04 -0300

    ProgWeb

---
+++ holamundo.php
@@ -0,0 +1,13 @@
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta name="description" content="">
+		<meta name="keywords" content="">
+		<meta name="author" content="">
+		<link rel="stylesheet" type="text/css" href="css.css">
+		<title>loco</title>
+	</head>
+	<body>
+
+	</body>
+</html>
